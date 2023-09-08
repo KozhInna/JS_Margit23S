919da95ab134f3b7f90544eecc30c3e40c6bfec8
Inna Kozhina <inna.kozhina22@gmail.com>
2023-09-09 00:07:17 +0300

    Finished JS part

--- task5_Pizza_app/main.js
+++ task5_Pizza_app/main.js
@@ -1,79 +1,72 @@
-/* let pizzaSize;
+let totalCost = 0;
+let pricePerSize = 0;
+let DeliveryCost = 0;
+let toppingCost = 0;
 
-document.querySelector('#sizeFor2').addEventListener('click', function () {
-  document.querySelector('.price').textContent = 7.5;
-  pizzaSize =
-});
-document.querySelector('#sizeFor4').addEventListener('click', function () {
-  document.querySelector('.price').textContent = 9;
-});
-document.querySelector('#sizeFor6').addEventListener('click', function () {
-  document.querySelector('.price').textContent = 10;
-});
-document.querySelector('#sizeFor8').addEventListener('click', function () {
-  document.querySelector('.price').textContent = 40;
-});
-console.log(pizzaSize);
-'7,50€, 4 - 10,50€, 6 - 12,50€, 8 15,50€' */
-
-/* Choosing a pizza size */
-let pricePerSize;
-
+/* Choosing a pizza size (radio buttons) */
 const sizes = document.querySelectorAll('.size');
-
 sizes.forEach((size) => {
   size.addEventListener('click', () => {
     if (size.value === 'serve2') {
-      document.querySelector('.price').textContent = '7,50 €';
       pricePerSize = 7.5;
+      totalCost = pricePerSize + toppingCost + DeliveryCost + ' €';
     } else if (size.value === 'serve4') {
-      document.querySelector('.price').textContent = '10,50 €';
       pricePerSize = 10.5;
+      totalCost = pricePerSize + toppingCost + DeliveryCost + ' €';
     } else if (size.value === 'serve6') {
-      document.querySelector('.price').textContent = '12,50 €';
       pricePerSize = 12.5;
+      totalCost = pricePerSize + toppingCost + DeliveryCost + ' €';
     } else {
-      document.querySelector('.price').textContent = '15,50 €';
       pricePerSize = 15.5;
+      totalCost = pricePerSize + toppingCost + DeliveryCost + ' €';
     }
+    document.querySelector('.price').textContent = totalCost;
   });
 });
+//console.log();
 
-/* Choosing toppings (first 4 - free, add. toppings - extra €0.50 per each)*/
+/* Choosing toppings (Checkboxes) (first 4 - free, add. toppings - extra €0.50 per each)*/
 const toppings = document.querySelectorAll('.topping');
 
 toppings.forEach((topping) => {
   topping.addEventListener('click', () => {
-    if (pricePerSize > 0) {
-      if (topping.value === 'serve2') {
-        document.querySelector('.price').textContent = '7,50 €';
-      } else if (topping.value === 'serve4') {
-        document.querySelector('.price').textContent = '10,50 €';
-      } else if (topping.value === 'serve6') {
-        document.querySelector('.price').textContent = '12,50 €';
-      } else {
-        document.querySelector('.price').textContent = '15,50 €';
+    let checkboxElems = document.getElementsByTagName('input');
+    let count = 0;
+    for (let i = 0; i < checkboxElems.length; i++) {
+      if (
+        checkboxElems[i].type === 'checkbox' &&
+        checkboxElems[i].checked === true
+      ) {
+        count++;
       }
+    }
+    // console.log(count);
+
+    /* adding price per size to topping */
+    if (count >= 4) {
+      toppingCost = (count - 4) * 0.5;
+      document.querySelector('.price').textContent =
+        pricePerSize + toppingCost + DeliveryCost + ' €';
     } else {
-      if (topping.value === 'serve2') {
-        document.querySelector('.price').textContent = '7,50 €';
-      } else if (topping.value === 'serve4') {
-        document.querySelector('.price').textContent = '10,50 €';
-      } else if (topping.value === 'serve6') {
-        document.querySelector('.price').textContent = '12,50 €';
-      } else {
-        document.querySelector('.price').textContent = '15,50 €';
-      }
+      document.querySelector('.price').textContent =
+        pricePerSize + toppingCost + DeliveryCost + ' €';
     }
   });
 });
-/* extraCheese
-sausage
-onion
-olives
-greenPepper
-garlic
-tomato
-basil
-mushrooms
-pineapple */
+
+/* deliver (dropdown menu) - home +5€, Eat in or Pickup food */
+// !! for "dropdown list" for Event we choose a <selector>, then add Event "change" (not "click"), then work with <option> values
+
+const deliveryMethod = document.querySelector('#select');
+
+deliveryMethod.addEventListener('change', function () {
+  if (deliveryMethod.value === 'home') {
+    DeliveryCost = 5;
+    document.querySelector('.price').textContent =
+      pricePerSize + toppingCost + DeliveryCost + ' €';
+  } else {
+    DeliveryCost = 0;
+    document.querySelector('.price').textContent =
+      DeliveryCost + toppingCost + pricePerSize + ' €';
+  }
+});
