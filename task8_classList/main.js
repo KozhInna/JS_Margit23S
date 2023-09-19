42819e1f3563d6968366e37700d5b6fa6aa2c900
Inna Kozhina <inna.kozhina22@gmail.com>
2023-09-19 13:24:40 +0300

    added @media, appearing menu

--- task8_classList/main.js
+++ task8_classList/main.js
@@ -1,9 +1,15 @@
 const goBackButton = document.querySelector("#backToTop");
+const mobButton = document.querySelector(".mobile");
+const nav = document.querySelector("nav ul");
+const menuItems = document.querySelectorAll("nav ul li a");
+const header = document.querySelector("header");
 
+//triger for scroll func
 window.onscroll = function () {
   scrollFunction();
 };
 
+// code from W#S about scrolling, for two browsers
 function scrollFunction() {
   if (
     document.body.scrollTop > 200 ||
@@ -13,10 +19,27 @@ function scrollFunction() {
   } else {
     goBackButton.style.display = "none";
   }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("bg");
+  } else {
+    header.classList.remove("bg");
+  }
 }
+const mobMenu = () => {
+  for (const item of menuItems) {
+    item.addEventListener("click", mobMenu);
+  }
+
+  if (nav.classList.contains("responsive")) {
+    nav.classList.remove("responsive");
+  } else {
+    nav.classList.add("responsive");
+  }
+};
 
 const getToTop = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; /* for 2nd broser */
 };
 goBackButton.addEventListener("click", getToTop);
+mobButton.addEventListener("click", mobMenu);
