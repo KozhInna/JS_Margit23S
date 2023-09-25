d3b254a5936cc1f550c253b0d11371b518df4b7b
Inna Kozhina <inna.kozhina22@gmail.com>
2023-09-25 12:54:06 +0300

    model window

--- task8_classList/main.js
+++ task8_classList/main.js
@@ -3,43 +3,73 @@ const mobButton = document.querySelector(".mobile");
 const nav = document.querySelector("nav ul");
 const menuItems = document.querySelectorAll("nav ul li a");
 const header = document.querySelector("header");
+const modalButton = document.querySelector("#modalBtn");
+const overlay = document.querySelector(".overlay");
+const xButton = document.querySelector(".x-btn");
 
 //triger for scroll func
 window.onscroll = function () {
   scrollFunction();
 };
 
-// code from W#S about scrolling, for two browsers
+// code from 3W School about scrolling, for two browsers
 function scrollFunction() {
-  if (
+  document.body.scrollTop > 200 || document.documentElement.scrollTop > 200
+    ? (goBackButton.style.display = "block")
+    : (goBackButton.style.display = "none");
+
+  /*  if (
     document.body.scrollTop > 200 ||
     document.documentElement.scrollTop > 200
   ) {
     goBackButton.style.display = "block";
   } else {
     goBackButton.style.display = "none";
-  }
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    header.classList.add("bg");
+  } */
+  document.body.scrollTop > 50 || document.documentElement.scrollTop > 50
+    ? header.classList.add("bg")
+    : header.classList.remove("bg");
+
+  /*   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("bg"); // class created in JS - we add it or remove it. properties are set in CSS
   } else {
     header.classList.remove("bg");
-  }
+  } */
 }
 const mobMenu = () => {
   for (const item of menuItems) {
     item.addEventListener("click", mobMenu);
   }
-
-  if (nav.classList.contains("responsive")) {
+  // class created in JS
+  /*   if (nav.classList.contains("responsive")) {
     nav.classList.remove("responsive");
   } else {
     nav.classList.add("responsive");
-  }
+  } */
+
+  /* ternary operator */
+  nav.classList.contains("responsive")
+    ? nav.classList.remove("responsive")
+    : nav.classList.add("responsive");
 };
 
 const getToTop = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; /* for 2nd broser */
 };
+
+const openModal = () => {
+  overlay.classList.toggle("visible");
+};
+
+/* const openModal = () => {
+  overlay.style.display = "block";
+};
+const closeModal = () => {
+  overlay.style.display = "none";
+};
+ */
 goBackButton.addEventListener("click", getToTop);
 mobButton.addEventListener("click", mobMenu);
+modalButton.addEventListener("click", openModal);
+xButton.addEventListener("click", openModal);
